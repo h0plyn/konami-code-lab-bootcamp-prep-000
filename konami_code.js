c3d15aa7ead28296e25da6c14a505527bb30f1ff
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-05 21:23:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', (event) => {
+    let key = event.key
+    
+    console.log(key)
+    
+    if (key === codes[index]) {
+      index++;
+      
+      console.log(index)
+      if (index === codes.length) {
+        alert('GODMODE UNLOCKED!')
+      };
+    } else {
+      
+      index = 0;
+    }
+  })
+  
 }
+
+init();
